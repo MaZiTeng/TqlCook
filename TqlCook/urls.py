4b5fdbe27e2ee3aead5a523af9806c168739ba72
ma <3075817585@qq.com>
2021-08-02 00:11:31 +0100

    project urls set

--- TqlCook/urls.py
+++ TqlCook/urls.py
@@ -5,3 +5,16 @@
 # @Site : 
 # @File : urls.py
 # @Software: PyCharm
+
+from django.urls import path
+from TqlCook import views
+
+
+
+urlpatterns = [
+    path('index/', views.home, name='home'),
+    path('search/', views.searchResult, name='searchResult'),
+    path('category/', views.category, name='category'),
+    path('recipe/', views.recipe, name='category'),
+    path('auth/', views.auth, name='category'),
+]
--- TqlCook/views.py
+++ TqlCook/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def searchResult(request):
+    return render(request, 'search.html')
+
+
+def category(request):
+    return render(request, 'category.html')
+
+
+def recipe(request):
+    return render(request, 'recipe.html')
+
+
+def auth(request):
+    return render(request, 'auth.html')
