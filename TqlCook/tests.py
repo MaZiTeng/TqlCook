68bd5cf47544a1fb6a2ec439055ee34e1f48e184
ma <3075817585@qq.com>
2021-08-07 05:13:54 +0100

    test

--- TqlCook/tests.py
+++ TqlCook/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from TqlCook.models import Recipe
 
 # Create your tests here.
+class TqlCookModelsTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Recipe.objects.create(id=9, name='Beef', ingredient='beef', method='heat', views=12, images="123.jpg")
+
+    def test_name_label(self):
+        recipe = Recipe.objects.get(id=9)
+        field_label=recipe._meta.get_field('name').verbose_name
+        self.assertEquals(field_label,'name')
+
+    def test_id_label(self):
+        recipe = Recipe.objects.get(id=9)
+        field_label = recipe._meta.get_field('id').verbose_name
+        self.assertEquals(field_label, 'id')
+
+    def test_ingredient_label(self):
+        recipe = Recipe.objects.get(id=9)
+        field_label=recipe._meta.get_field('ingredient').verbose_name
+        self.assertEquals(field_label,'ingredient')
+
+    def test_views_label(self):
+        recipe = Recipe.objects.get(id=9)
+        field_label=recipe._meta.get_field('views').verbose_name
+        self.assertEquals(field_label,'views')
